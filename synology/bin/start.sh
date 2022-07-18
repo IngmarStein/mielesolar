982b9e7670b5627007611efeafc12f2f7e530a12
Ingmar Stein <IngmarStein@users.noreply.github.com>
2022-07-19 00:24:49 +0200

    Log to designated location on Synology

--- synology/bin/start.sh
+++ synology/bin/start.sh
@@ -2,6 +2,7 @@
 
 SERVICE_EXEC_PATH="/var/packages/mielesolar/target/bin/mielesolar"
 CONFIG_FILE="/var/packages/mielesolar/target/mielesolar.conf"
+LOG_FILE="/var/packages/mielesolar/target/mielesolar.log"
 
 # Import config file
 # shellcheck disable=SC1090
@@ -11,4 +12,4 @@ exec "$SERVICE_EXEC_PATH" \
   -interval "${POLL_INTERVAL}" \
   -config "${CONFIG_FILE}" \
   -auto "${AUTO_POWER}" \
-  -vg "${COUNTRY_SELECTOR}"
+  -vg "${COUNTRY_SELECTOR}" > "${LOG_FILE}" 2>&1
