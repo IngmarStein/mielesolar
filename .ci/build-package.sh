350cc73b7401e583436e19ef6de91562c7c88e57
Ingmar Stein <IngmarStein@users.noreply.github.com>
2022-07-18 23:48:30 +0200

    Fix spk file paths

--- .ci/build-package.sh
+++ .ci/build-package.sh
@@ -9,4 +9,4 @@ cd synology
 tar cvfz package.tgz bin
 tar -c -v --exclude INFO.sh --exclude repo --exclude mielesolar*.spk \
   -f mielesolar-"${SPK_ARCH:-x86_64}"-"${SPK_PACKAGE_SUFFIX:-latest}".spk \
-  ./*
+  *
